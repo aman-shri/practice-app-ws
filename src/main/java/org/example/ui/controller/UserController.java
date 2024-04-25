4568f09f375ade7a35400b41eceb1bf659b42d66
aman1108 <shrivastava250@gmail.com>
2024-04-25 16:28:30 +0530

    added code to handle HTTP PUT requests

--- src/main/java/org/example/ui/controller/UserController.java
+++ src/main/java/org/example/ui/controller/UserController.java
@@ -2,6 +2,7 @@ package org.example.ui.controller;
 
 
 import jakarta.validation.Valid;
+import org.example.ui.model.request.UpdateUserDetailsRequestModel;
 import org.example.ui.model.request.UserDetailsRequestModel;
 import org.example.ui.model.response.UserRest;
 import org.springframework.http.HttpStatus;
@@ -59,9 +60,18 @@ public class UserController {
         return new ResponseEntity<>(returnValue, HttpStatus.OK);
     }
 
-    @PutMapping
-    public String updateUser() {
-        return "update user was called";
+    @PutMapping(path = "/{userId}",
+            consumes = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE},
+            produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
+    public UserRest updateUser(@PathVariable String userId, @Valid @RequestBody UpdateUserDetailsRequestModel userDetails) {
+
+        UserRest storedUserDetails = users.get(userId);
+        storedUserDetails.setFirstName(userDetails.getFirstName());
+        storedUserDetails.setLastName(userDetails.getLastName());
+
+        users.put(userId, storedUserDetails);
+
+        return storedUserDetails;
     }
 
     @DeleteMapping
