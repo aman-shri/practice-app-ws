3a4fede73bab81386dd487adb856bf8d1c69a8df
aman1108 <shrivastava250@gmail.com>
2024-04-25 17:44:48 +0530

    refactoring method to handle more than one exceptions

--- src/main/java/org/example/ui/exceptions/AppExceptionsHandler.java
+++ src/main/java/org/example/ui/exceptions/AppExceptionsHandler.java
@@ -28,8 +28,8 @@ public class AppExceptionsHandler extends ResponseEntityExceptionHandler {
     }
 
 
-    @ExceptionHandler(value = {NullPointerException.class})
-    public ResponseEntity<Object> handleNullPointerException(NullPointerException ex, WebRequest request) {
+    @ExceptionHandler(value = {NullPointerException.class, UserServiceException.class})
+    public ResponseEntity<Object> handleSpecificException(Exception ex, WebRequest request) {
 
         String errorMessageDescription = ex.getLocalizedMessage();
 
@@ -41,17 +41,4 @@ public class AppExceptionsHandler extends ResponseEntityExceptionHandler {
                 errorMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-
-    @ExceptionHandler(value = {UserServiceException.class})
-    public ResponseEntity<Object> handleUserServiceException(UserServiceException ex, WebRequest request) {
-
-        String errorMessageDescription = ex.getLocalizedMessage();
-
-        if(errorMessageDescription == null) errorMessageDescription = ex.toString();
-
-        ErrorMessage errorMessage = new ErrorMessage(new Date(), errorMessageDescription);
-
-        return new ResponseEntity<>(
-                errorMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
-    }
 }
