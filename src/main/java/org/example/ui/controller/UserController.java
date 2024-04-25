f8c393664277d697ca54641338b95b45229b7a30
aman1108 <shrivastava250@gmail.com>
2024-04-25 13:43:03 +0530

    added methods to handle HTTP requests

--- src/main/java/org/example/ui/controller/UserController.java
+++ src/main/java/org/example/ui/controller/UserController.java
@@ -1,12 +1,30 @@
 package org.example.ui.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping
+@RequestMapping("/users")  // http://localhost:8080/users
 public class UserController {
 
+    @GetMapping
+    public String getUser() {
+        return "get user was called";
+    }
+
+    @PostMapping
+    public String createUser() {
+        return "create user was called";
+    }
+
+    @PutMapping
+    public String updateUser() {
+        return "update user was called";
+    }
+
+    @DeleteMapping
+    public String deteleUser() {
+        return "delete user was called";
+    }
 
 }
